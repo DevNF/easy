e79104f4e48aca80cd98a4bcb29daf4bd2dfe91c
callseven <callseven@me.com>
2024-04-17 14:35:27 -0300

    Configurações iniciais

--- src/Tools.php
+++ src/Tools.php
@@ -2,7 +2,7 @@
 namespace NFHub\NFEasy;
 
 use Exception;
-use NFHub\Common\Tools as ToolsBase;
+
 /**
  * Classe Tools
  *
@@ -14,8 +14,173 @@ use NFHub\Common\Tools as ToolsBase;
  * @copyright 2024  Fuganholi Sistemas - NFSERVICE
  * @license   https://opensource.org/licenses/MIT MIT
  */
-class Tools extends ToolsBase
+class Tools
 {
+    /**
+     * URL base para comunicação com a API
+     *
+     * @var string
+     */
+    public static $API_URL = [
+        1 => 'https://api.fuganholi-easy.com.br/api',
+        2 => 'http://api.nfservice.com.br/api',
+        3 => 'https://api.sandbox.fuganholi-easy.com.br/api',
+        4 => 'https://api.dusk.fuganholi-easy.com.br/api'
+    ];
+
+    /**
+     * Variável responsável por armazenar os dados a serem utilizados para comunicação com a API
+     * Dados como token, ambiente(produção ou homologação) e debug(true|false)
+     *
+     * @var array
+     */
+    private $config = [
+        'token' => '',
+        'accesstoken' => '',
+        'environment' => '',
+        'debug' => false,
+        'upload' => false,
+        'decode' => true
+    ];
+
+    /**
+     * Define se a classe realizará um upload
+     *
+     * @param bool $isUpload Boleano para definir se é upload ou não
+     *
+     * @access public
+     * @return void
+     */
+    public function setUpload(bool $isUpload) :void
+    {
+        $this->config['upload'] = $isUpload;
+    }
+
+    /**
+     * Define se a classe realizará o decode do retorno
+     *
+     * @param bool $decode Boleano para definir se fa decode ou não
+     *
+     * @access public
+     * @return void
+     */
+    public function setDecode(bool $decode) :void
+    {
+        $this->config['decode'] = $decode;
+    }
+
+    /**
+     * Função responsável por definir se está em modo de debug ou não a comunicação com a API
+     * Utilizado para pegar informações da requisição
+     *
+     * @param bool $isDebug Boleano para definir se é produção ou não
+     *
+     * @access public
+     * @return void
+     */
+    public function setDebug(bool $isDebug) :void
+    {
+        $this->config['debug'] = $isDebug;
+    }
+
+    /**
+     * Função responsável por definir o token a ser utilizado para comunicação com a API
+     *
+     * @param string $token Token para autenticação na API
+     *
+     * @access public
+     * @return void
+     */
+    public function setToken(string $token) :void
+    {
+        $this->config['token'] = $token;
+    }
+
+     /**
+     * Função responsável por definir o accesstoken a ser utilizado para comunicação com a API
+     *
+     * @param string $accesstoken AccessToken para autenticação na API
+     *
+     * @access public
+     * @return void
+     */
+    public function setAccessToken(string $accesstoken) :void
+    {
+        $this->config['accesstoken'] = $accesstoken;
+    }
+
+    /**
+     * Função responsável por setar o ambiente utilizado na API
+     *
+     * @param int $environment Ambiente API (1 - Produção | 2 - Local | 3 - Sandbox | 4 - Dusk)
+     *
+     * @access public
+     * @return void
+     */
+    public function setEnvironment(int $environment) :void
+    {
+        if (in_array($environment, [1, 2, 3, 4])) {
+            $this->config['environment'] = $environment;
+        }
+    }
+
+    /**
+     * Recupera se é upload ou não
+     *
+     *
+     * @access public
+     * @return bool
+     */
+    public function getUpload() : bool
+    {
+        return $this->config['upload'];
+    }
+
+    /**
+     * Recupera se faz decode ou não
+     *
+     *
+     * @access public
+     * @return bool
+     */
+    public function getDecode() : bool
+    {
+        return $this->config['decode'];
+    }
+
+    /**
+     * Recupera o ambiente setado para comunicação com a API
+     *
+     * @access public
+     * @return int
+     */
+    public function getEnvironment() :int
+    {
+        return $this->config['environment'];
+    }
+
+    /**
+     * Retorna os cabeçalhos padrão para comunicação com a API
+     *
+     * @access private
+     * @return array
+     */
+    private function getDefaultHeaders() :array
+    {
+        $headers = [
+            'Authorization: Bearer '.$this->config['token'],
+            'Access-Token: '.$this->config['accesstoken'],
+            'Accept: application/json',
+        ];
+
+        if (!$this->config['upload']) {
+            $headers[] = 'Content-Type: application/json';
+        } else {
+            $headers[] = 'Content-Type: multipart/form-data';
+        }
+
+        return $headers;
+    }
 
     /**
      * Lista os Bancos cadastrados no Easy
@@ -75,7 +240,7 @@ class Tools extends ToolsBase
                 ];
             }
 
-            $dados = $this->get('/listBanks', $params);
+            $dados = $this->get('/listCategories', $params);
 
             if (!isset($dados['body']->message)) {
                 return $dados;
@@ -85,5 +250,223 @@ class Tools extends ToolsBase
         } catch (Exception $error) {
             throw new Exception($error, 1);
         }
+    }
+
+    /**
+     * Execute a GET Request
+     *
+     * @param string $path
+     * @param array $params
+     * @param array $headers Cabeçalhos adicionais para requisição
+     *
+     * @access protected
+     * @return array
+     */
+    protected function get(string $path, array $params = [], array $headers = []) :array
+    {
+        $opts = [
+            CURLOPT_HTTPHEADER => $this->getDefaultHeaders()
+        ];
+
+        if (!empty($headers)) {
+            $opts[CURLOPT_HTTPHEADER] = array_merge($opts[CURLOPT_HTTPHEADER], $headers);
+        }
+
+        $exec = $this->execute($path, $opts, $params);
+
+        return $exec;
+    }
+
+    /**
+     * Execute a POST Request
+     *
+     * @param string $path
+     * @param string $body
+     * @param array $params
+     * @param array $headers Cabeçalhos adicionais para requisição
+     *
+     * @access protected
+     * @return array
+     */
+    protected function post(string $path, array $body = [], array $params = [], array $headers = []) :array
+    {
+        $opts = [
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => !$this->config['upload'] ? json_encode($body) : $this->convertToFormData($body),
+            CURLOPT_HTTPHEADER => $this->getDefaultHeaders()
+        ];
+
+        if (!empty($headers)) {
+            $opts[CURLOPT_HTTPHEADER] = array_merge($opts[CURLOPT_HTTPHEADER], $headers);
+        }
+
+        $exec = $this->execute($path, $opts, $params);
+
+        return $exec;
+    }
+
+    /**
+     * Execute a PUT Request
+     *
+     * @param string $path
+     * @param string $body
+     * @param array $params
+     * @param array $headers Cabeçalhos adicionais para requisição
+     *
+     * @access protected
+     * @return array
+     */
+    protected function put(string $path, array $body = [], array $params = [], array $headers = []) :array
+    {
+        $opts = [
+            CURLOPT_HTTPHEADER => $this->getDefaultHeaders(),
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            CURLOPT_POSTFIELDS => json_encode($body)
+        ];
+
+        if (!empty($headers)) {
+            $opts[CURLOPT_HTTPHEADER] = array_merge($opts[CURLOPT_HTTPHEADER], $headers);
+        }
+
+        $exec = $this->execute($path, $opts, $params);
+
+        return $exec;
+    }
+
+    /**
+     * Execute a DELETE Request
+     *
+     * @param string $path
+     * @param array $params
+     * @param array $headers Cabeçalhos adicionais para requisição
+     *
+     * @access protected
+     * @return array
+     */
+    protected function delete(string $path, array $params = [], array $headers = []) :array
+    {
+        $opts = [
+            CURLOPT_HTTPHEADER => $this->getDefaultHeaders(),
+            CURLOPT_CUSTOMREQUEST => "DELETE"
+        ];
+
+        if (!empty($headers)) {
+            $opts[CURLOPT_HTTPHEADER] = array_merge($opts[CURLOPT_HTTPHEADER], $headers);
+        }
+
+        $exec = $this->execute($path, $opts, $params);
+
+        return $exec;
+    }
+
+    /**
+     * Execute a OPTION Request
+     *
+     * @param string $path
+     * @param array $params
+     * @param array $headers Cabeçalhos adicionais para requisição
+     *
+     * @access protected
+     * @return array
+     */
+    protected function options(string $path, array $params = [], array $headers = []) :array
+    {
+        $opts = [
+            CURLOPT_CUSTOMREQUEST => "OPTIONS"
+        ];
+
+        if (!empty($headers)) {
+            $opts[CURLOPT_HTTPHEADER] = $headers;
+        }
+
+        $exec = $this->execute($path, $opts, $params);
+
+        return $exec;
+    }
+
+    /**
+     * Função responsável por realizar a requisição e devolver os dados
+     *
+     * @param string $path Rota a ser acessada
+     * @param array $opts Opções do CURL
+     * @param array $params Parametros query a serem passados para requisição
+     *
+     * @access protected
+     * @return array
+     */
+    protected function execute(string $path, array $opts = [], array $params = []) :array
+    {
+        if (!preg_match("/^\//", $path)) {
+            $path = '/' . $path;
+        }
+
+        $url = self::$API_URL[$this->config['environment']].$path;
+
+        $curlC = curl_init();
+
+        if (!empty($opts)) {
+            curl_setopt_array($curlC, $opts);
+        }
+
+        if (!empty($params)) {
+            $paramsJoined = [];
+
+            foreach ($params as $param) {
+                if (isset($param['name']) && !empty($param['name']) && isset($param['value']) && !empty($param['value'])) {
+                    $paramsJoined[] = urlencode($param['name'])."=".urlencode($param['value']);
+                }
+            }
+
+            if (!empty($paramsJoined)) {
+                $params = '?'.implode('&', $paramsJoined);
+                $url = $url.$params;
+            }
+        }
+
+        curl_setopt($curlC, CURLOPT_URL, $url);
+        curl_setopt($curlC, CURLOPT_RETURNTRANSFER, true);
+        if (!empty($dados)) {
+            curl_setopt($curlC, CURLOPT_POSTFIELDS, json_encode($dados));
+        }
+
+        $retorno = curl_exec($curlC);
+        $info = curl_getinfo($curlC);
+        $return["body"] = ($this->config['decode'] || !$this->config['decode'] && $info['http_code'] != '200') ? json_decode($retorno) : $retorno;
+        $return["httpCode"] = curl_getinfo($curlC, CURLINFO_HTTP_CODE);
+        if ($this->config['debug']) {
+            $return['info'] = curl_getinfo($curlC);
+        }
+        curl_close($curlC);
+
+        return $return;
+    }
+
+    /**
+     * Função responsável por montar o corpo de uma requisição no formato aceito pelo FormData
+     */
+    private function convertToFormData($data)
+    {
+        $dados = [];
+
+        $recursive = false;
+        foreach ($data as $key => $value) {
+            if (!is_array($value)) {
+                $dados[$key] = $value;
+            } else {
+                foreach ($value as $subkey => $subvalue) {
+                    $dados[$key.'['.$subkey.']'] = $subvalue;
+
+                    if (is_array($subvalue)) {
+                        $recursive = true;
+                    }
+                }
+            }
+        }
+
+        if ($recursive) {
+            return $this->convertToFormData($dados);
+        }
+
+        return $dados;
     }
 }
